011d9bdeb121ee3c3f6b65a792ff9acfdcc18c25
Snorre Eskeland Brekke <snorre.e.brekke@gmail.com>
2022-03-10 22:33:17 +0100

    feat: use checkbox instead of button for edit enable

--- src/components/document-wrapper/ScheduledDocumentInput.tsx
+++ src/components/document-wrapper/ScheduledDocumentInput.tsx
@@ -1,12 +1,13 @@
 import {SanityProps} from './helper-types'
-import React, {forwardRef, Ref, useMemo, useState} from 'react'
+import React, {forwardRef, Ref, useCallback, useMemo, useState} from 'react'
 import {NestedFormBuilder} from './NestedFormBuilder'
 import {ObjectSchemaType} from '@sanity/types'
 import usePollSchedules from '../../hooks/usePollSchedules'
 import useTimeZone from '../../hooks/useTimeZone'
 import formatDateTz from '../../utils/formatDateTz'
-import {Badge, Box, Button, Card, Flex, Stack, Text} from '@sanity/ui'
-import {EditIcon, ErrorOutlineIcon} from '@sanity/icons'
+import {Badge, Box, Card, Flex, Stack, Switch} from '@sanity/ui'
+import {useId} from '@reach/auto-id'
+import {FormFieldHeaderText} from '@sanity/base/components'
 
 export const scheduledMarkerFieldName = 'hasScheduleWrapper'
 export const validationMarkerField = '_validationError'
@@ -28,6 +29,8 @@ export const ScheduledDocumentInput = forwardRef(function ScheduledDocumentInput
   const hasError = useMemo(() => props.markers.some((m) => m.level === 'error'), [props.markers])
 
   const [editScheduled, setEditScheduled] = useState(false)
+  const toggleEdit = useCallback(() => setEditScheduled((current) => !current), [setEditScheduled])
+
   const readOnly = props.readOnly || (!!formattedDateTime && !editScheduled)
   return (
     <>
@@ -40,27 +43,9 @@ export const ScheduledDocumentInput = forwardRef(function ScheduledDocumentInput
               </Box>
               <Box>{formattedDateTime}</Box>
 
-              {hasError && (
-                <Card shadow={1} padding={4}>
-                  <Flex gap={2} justify="center" align="center">
-                    <Text size={2}>
-                      <ErrorOutlineIcon color="red" />
-                    </Text>
-                    <Text>Validation errors that should be fixed before the scheduled time!</Text>
-                  </Flex>
-                </Card>
-              )}
-
-              {!editScheduled && (
-                <Flex flex={1} justify="center" marginTop={2}>
-                  <Button
-                    icon={EditIcon}
-                    text={'Enable editing'}
-                    style={{width: '100%'}}
-                    onClick={() => setEditScheduled(true)}
-                  />
-                </Flex>
-              )}
+              <Box marginTop={2}>
+                <EnableEditing enabled={editScheduled} onClick={toggleEdit} hasError={hasError} />
+              </Box>
             </Stack>
           </Card>
         </Box>
@@ -69,6 +54,45 @@ export const ScheduledDocumentInput = forwardRef(function ScheduledDocumentInput
     </>
   )
 })
+
+function EnableEditing({
+  enabled,
+  onClick,
+  hasError,
+}: {
+  enabled: boolean
+  onClick: () => void
+  hasError: boolean
+}) {
+  const inputId = useId()
+  return (
+    <Card border radius={1} flex={1} padding={2}>
+      <Flex align="center" gap={2}>
+        <Box>
+          <Switch id={inputId} checked={enabled} onChange={onClick} />
+        </Box>
+        <Box flex={1} paddingY={3}>
+          <FormFieldHeaderText
+            title="Enable editing"
+            description={
+              hasError ? (
+                <>
+                  <strong>This document has errors.</strong>
+                  <br />
+                  It will still be published according to schedule. Correcting these errors is
+                  strongly recommended.
+                </>
+              ) : (
+                'This draft will be published according to schedule. Any changes made will be part of the published document.'
+              )
+            }
+            inputId={inputId}
+          />
+        </Box>
+      </Flex>
+    </Card>
+  )
+}
 
 function useNextSchedule(id?: string) {
   const {schedules} = usePollSchedules({documentId: id, state: 'scheduled'})
