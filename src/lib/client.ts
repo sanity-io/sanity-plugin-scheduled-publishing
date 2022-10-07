4b571263c4839dd24af39bfa5deb5517ddd11593
Robin Pyon <robin.pyon@gmail.com>
2022-10-07 09:30:02 +0100

    fix: use the studio client to handle cookieless auth (#25)

--- src/lib/client.ts
+++ src/lib/client.ts
@@ -1,8 +1,10 @@
-import sanityClient from '@sanity/client'
-import configuredClient from 'part:@sanity/base/client'
+import type {SanityClient} from '@sanity/client'
+import sanityClient from 'part:@sanity/base/client'
 import {SANITY_API_VERSION} from '../constants'
 
 // Returns as a function so the currently selected Space dataset + projectId are used
 export function getSanityClient() {
-  return sanityClient({...configuredClient.config(), apiVersion: SANITY_API_VERSION})
+  return typeof sanityClient.withConfig === 'function'
+    ? (sanityClient.withConfig({apiVersion: SANITY_API_VERSION}) as SanityClient)
+    : (sanityClient as SanityClient)
 }
