8fc4a3d13c838f3887ab756391452ae990064422
Shyen Wee <swee@tictoc.com.au>
2024-01-17 14:16:54 +1030

    refactor(NewScheduleInfo): update text and toggle functionality for clarity

--- src/documentActions/schedule/NewScheduleInfo.tsx
+++ src/documentActions/schedule/NewScheduleInfo.tsx
@@ -1,4 +1,4 @@
-import {Card, Flex, Stack, Text, Switch, Label} from '@sanity/ui'
+import {Card, Flex, Stack, Text, Switch} from '@sanity/ui'
 import React from 'react'
 import {usePublishedId} from '../../hooks/usePublishedId'
 import {useSchemaType} from '../../hooks/useSchemaType'
@@ -18,22 +18,33 @@ interface Props {
 export function NewScheduleInfo({id, schemaType, onActionTypeChange, actionType}: Props) {
   return (
     <Stack space={4}>
-      {/* Switch component to toggle between publish and unpublish */}
-      <Label htmlFor="actionTypeToggle" size={1} style={{alignSelf: 'flex-start'}}>
-        Schedule to {actionType === 'publish' ? 'Publish' : 'Unpublish'}
-      </Label>
-      <Switch
-        id="actionTypeToggle"
-        checked={actionType === 'unpublish'}
-        onChange={onActionTypeChange}
-      />
+      <Text size={1} weight="semibold">
+        Toggle Publish/Unpublish
+      </Text>
+
+      <Flex align="center" gap={2}>
+        <Text size={1} muted={actionType === 'unpublish'}>
+          Publish
+        </Text>
+        <Switch
+          id="actionTypeToggle"
+          checked={actionType === 'unpublish'}
+          onChange={onActionTypeChange}
+        />
+        <Text size={1} muted={actionType === 'publish'}>
+          Unpublish
+        </Text>
+      </Flex>
+
       <Text size={1}>
         Schedule this document to be {actionType === 'publish' ? 'published' : 'unpublished'} at any
         time in the future.
         <br />
         Any edits in the meantime will be added to the scheduled document.
       </Text>
+
       <Text size={1}>Visit the Schedules page to get an overview of all schedules.</Text>
+
       <ValidationWarning id={id} type={schemaType} />
     </Stack>
   )
