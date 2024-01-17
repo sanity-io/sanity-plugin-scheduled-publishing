5bf6fd333e47936cace8763982eba278e6651cd3
Shyen Wee <swee@tictoc.com.au>
2024-01-17 12:50:35 +1030

    feat(NewScheduleInfo): add toggle for publish/unpublish action selection

--- src/documentActions/schedule/NewScheduleInfo.tsx
+++ src/documentActions/schedule/NewScheduleInfo.tsx
@@ -1,4 +1,4 @@
-import {Card, Flex, Stack, Text} from '@sanity/ui'
+import {Card, Flex, Stack, Text, Switch, Label} from '@sanity/ui'
 import React from 'react'
 import {usePublishedId} from '../../hooks/usePublishedId'
 import {useSchemaType} from '../../hooks/useSchemaType'
@@ -6,17 +6,30 @@ import {useValidationStatus} from 'sanity'
 import {useValidationState} from '../../utils/validationUtils'
 import {ValidationInfo} from '../../components/validation/ValidationInfo'
 import {DOCUMENT_HAS_ERRORS_TEXT} from '../../constants'
+import {ScheduleAction} from '../../types'
 
 interface Props {
   id: string
   schemaType: string
+  onActionTypeChange: (event: React.ChangeEvent<HTMLInputElement>) => void
+  actionType: ScheduleAction
 }
 
-export function NewScheduleInfo({id, schemaType}: Props) {
+export function NewScheduleInfo({id, schemaType, onActionTypeChange, actionType}: Props) {
   return (
     <Stack space={4}>
+      {/* Switch component to toggle between publish and unpublish */}
+      <Label htmlFor="actionTypeToggle" size={1} style={{alignSelf: 'flex-start'}}>
+        Schedule to {actionType === 'publish' ? 'Publish' : 'Unpublish'}
+      </Label>
+      <Switch
+        id="actionTypeToggle"
+        checked={actionType === 'unpublish'}
+        onChange={onActionTypeChange}
+      />
       <Text size={1}>
-        Schedule this document to be published at any time in the future.
+        Schedule this document to be {actionType === 'publish' ? 'published' : 'unpublished'} at any
+        time in the future.
         <br />
         Any edits in the meantime will be added to the scheduled document.
       </Text>
